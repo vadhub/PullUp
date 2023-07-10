6d72c015dfa72014e5c100a9969622ef9dda1f17
vadim <gabderahmanov99@gmail.com>
2023-07-10 14:31:32 +0300

    configuration

---
+++ app/src/main/java/com/vad/pullup/data/Configuration.kt
@@ -0,0 +1,34 @@
+package com.vad.pullup.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Configuration(private val context: Context) {
+
+    private lateinit var pref: SharedPreferences
+
+    fun saveDay(day: Int) {
+        pref = context.getSharedPreferences("pull_up_day", Context.MODE_PRIVATE)
+        val ed: SharedPreferences.Editor = pref.edit()
+        ed.putInt("day", day)
+        ed.apply()
+    }
+
+    fun getDay(): Int {
+        pref = context.getSharedPreferences("pull_up_day", Context.MODE_PRIVATE)
+        return pref.getInt("day",0)
+    }
+
+    fun saveFirstStart(isFirst: Boolean) {
+        pref = context.getSharedPreferences("pull_up_first_run", Context.MODE_PRIVATE)
+        val ed: SharedPreferences.Editor = pref.edit()
+        ed.putBoolean("first_run", isFirst)
+        ed.apply()
+    }
+
+    fun getFirstStart(): Boolean {
+        pref = context.getSharedPreferences("pull_up_first_run", Context.MODE_PRIVATE)
+        return pref.getBoolean("first_run", false)
+    }
+
+}
