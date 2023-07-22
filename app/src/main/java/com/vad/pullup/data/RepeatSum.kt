6b502348d3205eafbe7b47db0cf326aa570cf509
vadim <gabderahmanov99@gmail.com>
2023-07-22 16:36:12 +0300

    correct "Cannot figure out how to read this field from a cursor"

--- app/src/main/java/com/vad/pullup/data/RepeatSum.kt
+++ app/src/main/java/com/vad/pullup/data/RepeatSum.kt
@@ -1,8 +1,10 @@
 package com.vad.pullup.data
 
+import androidx.room.TypeConverters
+import com.vad.pullup.data.db.DateConvertor
 import java.sql.Date
 
-
+@TypeConverters(DateConvertor::class)
 data class RepeatSum(
     val countRepeat: Int,
     val dateRepeat: Date
