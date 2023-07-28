63335c9a339e9f01030b657be5672b48a2c1bfc1
vadim <gabderahmanov99@gmail.com>
2023-07-28 15:00:11 +0300

    format date

--- app/src/main/java/com/vad/pullup/ui/StatisticFragment.kt
+++ app/src/main/java/com/vad/pullup/ui/StatisticFragment.kt
@@ -1,5 +1,6 @@
 package com.vad.pullup.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.vad.pullup.R
 import com.vad.pullup.BaseFragment
+import java.text.SimpleDateFormat
 
 class StatisticFragment : BaseFragment() {
 
@@ -22,6 +24,7 @@ class StatisticFragment : BaseFragment() {
         return inflater.inflate(R.layout.fragment_statistic, container, false)
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val chart = view.findViewById(R.id.chart) as LineChart
 
@@ -30,13 +33,13 @@ class StatisticFragment : BaseFragment() {
 
         chart.axisRight.textSize = 16f
         yAxis.textSize = 16f
-
+        val simpleDateFormat = SimpleDateFormat("MM.dd")
         exerciseViewModel.getSumRepeat()
 
         exerciseViewModel.sumRepeat.observe(viewLifecycleOwner) { repeat ->
             Log.d("$1", repeat.toTypedArray().contentToString())
 
-            val dates = repeat.map { "${it.dateRepeat}" }
+            val dates = repeat.map { simpleDateFormat.format(it.dateRepeat) }
             val xAxis = chart.xAxis
             xAxis.textSize = 16f
             xAxis.granularity = 1f
