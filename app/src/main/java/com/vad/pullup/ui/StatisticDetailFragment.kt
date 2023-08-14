f6b1a49a1fba5e499d7041d5daaeef6e7d6dd6f5
vadim <gabderahmanov99@gmail.com>
2023-08-14 13:04:22 +0300

    plug if empty data

--- app/src/main/java/com/vad/pullup/ui/StatisticDetailFragment.kt
+++ app/src/main/java/com/vad/pullup/ui/StatisticDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vad.pullup.BaseFragment
@@ -20,11 +21,15 @@ class StatisticDetailFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val emptyText = view.findViewById<TextView>(R.id.emptyTextView)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewDetail)
         recyclerView.layoutManager = LinearLayoutManager(thisContext)
 
         exerciseViewModel.getAllExercise()
         exerciseViewModel.allExercise.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                emptyText.visibility = View.GONE
+            }
             val listExerciseByDate = it.groupBy { it.date.toString() }.toList()
             val adapter = ExerciseAdapter(listExerciseByDate)
             recyclerView.adapter = adapter
