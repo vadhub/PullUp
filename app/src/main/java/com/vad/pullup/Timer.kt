730d6a2bb10c03575c0466ee420e04b5bc04ab7c
vadim <gabderahmanov99@gmail.com>
2024-02-25 23:31:04 +0300

    timer TDD

---
+++ app/src/main/java/com/vad/pullup/Timer.kt
@@ -0,0 +1,43 @@
+package com.vad.pullup
+
+class Timer {
+
+    private var timerListener: TimerListener? = null
+
+    private var milliseconds = 0L
+
+    var isStart = false
+        private set
+
+    fun start() {
+        isStart = true
+    }
+
+    fun stop() {
+        isStart = false
+    }
+
+    fun pause() {
+        isStart = false
+    }
+
+    fun restart() {
+        isStart = true
+    }
+
+    fun setTimerInMilliseconds(milliseconds: Long) {
+        this.milliseconds = milliseconds
+    }
+
+    fun getMilliseconds() = milliseconds
+
+    fun setTimerListener(timerListener: TimerListener) {
+        this.timerListener = timerListener
+    }
+
+    fun getTimerListener() = timerListener
+
+    interface TimerListener {
+
+    }
+}
---
+++ app/src/test/java/com/vad/pullup/TimerUnitTest.kt
@@ -0,0 +1,59 @@
+package com.vad.pullup
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class TimerUnitTest {
+
+    private val timer by lazy { Timer() }
+
+    @Test
+    fun isStartTimer() {
+        val isStart = timer.isStart
+        assertEquals(false, isStart)
+    }
+
+    @Test
+    fun startTimer() {
+        timer.start()
+        val isStart = timer.isStart
+        assertEquals(true, isStart)
+    }
+
+    @Test
+    fun stopTimer() {
+        timer.stop()
+        val isStart = timer.isStart
+        assertEquals(false, isStart)
+    }
+
+    @Test
+    fun pauseTimer() {
+        timer.pause()
+        val isStart = timer.isStart
+        assertEquals(false, isStart)
+    }
+
+    @Test
+    fun restartTimer() {
+        timer.restart()
+        val isStart = timer.isStart
+        assertEquals(true, isStart)
+    }
+
+    @Test
+    fun setTime_one_second() {
+        timer.setTimerInMilliseconds(1000)
+        val milliseconds = timer.getMilliseconds()
+        assertEquals(1000, milliseconds)
+    }
+
+    @Test
+    fun setTimerListener() {
+        val timerListener: Timer.TimerListener = object : Timer.TimerListener {}
+        timer.setTimerListener(timerListener)
+        assertNotNull(timer.getTimerListener())
+    }
+
+}
